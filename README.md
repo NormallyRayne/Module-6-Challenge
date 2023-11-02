5d5e65b03ca349fa63f6df7de5ad476d5e656649
Rick Collett <ricksmacbookair@Ricks-Air.attlocal.net>
2023-11-02 11:52:01 -0400

    Finished Script.js

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# Module-6-Challenge
+# Weather-Dashboard
+
+## Description
+
+I was inspired to develop a weather application capable of accommodating various location inputs. This project was designed to assist individuals seeking weather forecasts tailored to specific locations, addressing the inconvenience of having to refresh a page whenever someone desired weather information for a different location. Through this endeavor, I gained valuable experience in utilizing APIs to present precise data within an application.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+N/A
+
+## Usage
+
+Here is the link to deployed page: []
+
+## Credits
+
+Rick Collett - https://github.com/NormallyRayne
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+
+## Features
+
+Website
+![image] /Users/ricksmacbookair/Documents/Development/School /Module-6-Challenge/assets/images/Weather Dashboard.png
+
+
+## How to Contribute
+
+Rick Collett
+
---
+++ assets/js/script.js
@@ -0,0 +1,84 @@
+var apiKey = "4ae18b140d65ec8337ba26b980dc1ef6"
+
+var titleEl = document.getElementById("title")
+var tempEl = document.getElementById("temp")
+var windEl = document.getElementById("wind")
+var humidityEl = document.getElementById("humidity")
+var searchBtn = document.getElementById("search-btn")
+var cityInput = document.getElementById("city-input")
+var fivedayForecast = document.getElementById("fiveday-forecast")
+var buttonGroup = document.getElementById("buttongroup-container")
+
+
+function searchCity() {
+    var cityName = cityInput.value
+
+    displayWeather(cityName)
+
+}
+
+function displayWeather(cityName) {
+    var url = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + apiKey + "&units=imperial"
+
+    fetch(url)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (currentData) {
+            console.log(currentData)
+            titleEl.innerHTML = currentData.name + dayjs.unix(currentData.dt).format(" (MM/DD/YYYY)") + "<img src='https://openweathermap.org/img/wn/" + currentData.weather[0].icon + "@2x.png'>"
+            tempEl.textContent = `Temp: ${currentData.main.temp} F`
+            windEl.textContent = `Wind Speed: ${currentData.wind.speed} MPH`
+            humidityEl.textContent = `Humidity: ${currentData.main.humidity}`
+        })
+
+
+    var forecastUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&units=imperial"
+    fetch(forecastUrl)
+        .then(function (response) {
+            return response.json()
+        })
+        .then(function (forecastData) {
+            console.log(forecastData)
+
+            // grab every 12pm for each day for 5 days
+            var forecastListArr = forecastData.list
+
+            for (let i = 2, j = 1; i <= forecastListArr.length; i = i + 8, j++) {
+                // console.log(forecastListArr[i].dt_txt)
+
+                var cardTitle = document.getElementById("card-title" + j)
+                cardTitle.textContent = dayjs(forecastListArr[i].dt_txt).format(" (MM/DD/YYYY)")
+
+                var iconEl = document.getElementById("icon" + j)
+                iconEl.src = `https://openweathermap.org/img/wn/${forecastListArr[i].weather[0].icon}@2x.png`
+
+                var fiveDayTemp = document.getElementById("temp" + j)
+                fiveDayTemp.textContent = `Temp: ${forecastListArr[i].main.temp} F`
+
+                var fiveDayWind = document.getElementById("wind" + j)
+                fiveDayWind.textContent = `Wind Speed: ${forecastListArr[i].wind.speed} MPH`
+
+                var fiveDayHumidity = document.getElementById("humidity" + j)
+                fiveDayHumidity.textContent = `Humidity: ${forecastListArr[i].main.humidity}`
+
+            }
+        })
+}
+//takes innerHTML of selected button and uses it in display weather function
+function cityButton(i) {
+    displayWeather(i)
+}
+
+
+//click event using search button
+searchBtn.addEventListener("click", searchCity)
+
+
+//click event for button with city names on them
+buttonGroup.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-secondary')) {
+        cityButton(e.target.innerHTML)
+    }
+})
+
